d1f0c2a771e5fc2a65959e1e22dfffe7a856c32e
Michelle Clement <michellelclement@gmail.com>
2022-07-14 13:33:24 +0000

    Contact form added

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,11 @@
+function sendMail(contactForm) {
+  emailjs
+    .send("gmail", "bottle_and_baby", {
+      from_name: contactForm.contactName.value,
+      from_email: contactForm.contactEmail.value,
+      message: contactForm.message.value,
+    })
+    .then(alert("Thank you! Your message has been sent. We will get back to you as soon as we can."));
+
+  $("#contact-form").text();
+}
