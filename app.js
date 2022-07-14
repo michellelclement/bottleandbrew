fc0b9939c5571bfcee37ebb8b29321a9eb3af971
Michelle Clement <michellelclement@gmail.com>
2022-07-14 08:19:13 +0000

    Map now displaying full mobile screen

---
+++ app.js
@@ -0,0 +1,6 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiYm90dGxlYW5kYnJld2JhYnkiLCJhIjoiY2w0NW5qc3FlMDAzdTNkcDBkaG4xNXMyeCJ9.Tscd2VjIKdHJb1m7AHb8IQ';
+var map = new mapboxgl.Map({
+container: 'map',
+cooperativeGestures: true,
+style: 'mapbox://styles/bottleandbrewbaby/cl4oa3t0y000p16mh5ciqw6gb'
+});
