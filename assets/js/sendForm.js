c362711787c3372bd341665d212b1478dced320b
Michelle Clement <michellelclement@gmail.com>
2022-07-19 08:55:26 +0000

    Both forms added and working

---
+++ assets/js/sendForm.js
@@ -0,0 +1,14 @@
+function sendForm(contactForm) {
+  emailjs
+    .send("gmail", "bottle_and_baby_add_venu", {
+      from_name: contactForm.addVenueContactName.value,
+      from_email: contactForm.addVenueEmail.value,
+      venue_comments: contactForm.venueComments.value,
+      venue_name: contactForm.venueName.value,
+      venue_address: contactForm.venueAddress.value,
+      venue_website: contactForm.venueWebsite.value,
+    })
+    .then(alert("Thank you! We will add your venue submission as soon as we can."));
+
+  $("#contact-form").text();
+}
